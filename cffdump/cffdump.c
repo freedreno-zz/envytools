00aba6959b31ee27559246c9d2acbdb217452c3f
Rob Clark <robdclark@chromium.org>
2020-02-14 09:17:57 -0800

    cffdump: split out pager
    Want to re-use this for crashdec.

--- cffdump/cffdump.c
+++ cffdump/cffdump.c
@@ -42,6 +42,7 @@
 #include "script.h"
 #include "io.h"
 #include "rnnutil.h"
+#include "pager.h"
 
 /* ************************************************************************* */
 /* originally based on kernel recovery dump code: */
@@ -2545,68 +2546,6 @@ static void print_usage(const char *name)
 	printf("    --disasm/-d       - combine with query mode, disassembles shader referenced\n");
 	printf("                        by queried register\n");
 	printf("    --help            - show this message\n");
-}
-
-
-static pid_t pager_pid;
-
-static void pager_death(int n)
-{
-	exit(0);
-}
-
-static void pager_open(void)
-{
-	int fd[2];
-
-	if (pipe(fd) < 0) {
-		fprintf(stderr, "Failed to create pager pipe: %m\n");
-		exit(-1);
-	}
-
-	pager_pid = fork();
-	if (pager_pid < 0) {
-		fprintf(stderr, "Failed to fork pager: %m\n");
-		exit(-1);
-	}
-
-	if (pager_pid == 0) {
-		const char* less_opts;
-
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-
-		less_opts = "FRSMKX";
-		setenv("LESS", less_opts, 1);
-
-		execlp("less", "less", NULL);
-
-	} else {
-		/* we want to kill the parent process when pager exits: */
-		signal(SIGCHLD, pager_death);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-	}
-}
-
-static int pager_close(void)
-{
-	siginfo_t status;
-
-	close(STDOUT_FILENO);
-
-	while (true) {
-		memset(&status, 0, sizeof(status));
-		if (waitid(P_PID, pager_pid, &status, WEXITED) < 0) {
-			if (errno == EINTR)
-				continue;
-			return -errno;
-		}
-
-		return 0;
-	}
 }
 
 int main(int argc, char **argv)
